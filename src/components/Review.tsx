6c01425fc5bddf2225a28b612c91068345c146db
tiwariParth <243mjparth@gmail.com>
2024-06-16 18:21:44 +0530

    chore: Update Review component with lazy loading for images

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import React, { useRef } from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+
+const ReviewGrid = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  return <div ref={containerRef} className="relative -mx-4 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
+     
+  </div>;
+};
 
 const Review = () => {
   return (
-    <div>Review</div>
-  )
-}
+    <MaxWidthWrapper className="relative max-w-5xl">
+      <img
+        src="/what-people-are-buying.png"
+        alt=""
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
 
-export default Review
+      <ReviewGrid />
+    </MaxWidthWrapper>
+  );
+};
+
+export default Review;
