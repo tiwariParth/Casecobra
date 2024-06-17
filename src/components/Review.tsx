352da089c5f0184c03e4c0989af0d500ef0226a0
tiwariParth <243mjparth@gmail.com>
2024-06-17 10:02:45 +0530

    chore: Update framer-motion dependency to version 11.2.10

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,12 +1,66 @@
 "use client";
 import React, { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
+  return result;
+}
 
 const ReviewGrid = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  return <div ref={containerRef} className="relative -mx-4 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">
-     
-  </div>;
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
+  const columns = splitArray(PHONES, 3);
+  const columns1 = columns[0];
+  const columns2 = columns[1];
+  const columns3 = splitArray(columns[2], 2);
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {isInView ? (
+        <>
+          <ReviewColumn />
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+const ReviewColumn = ({
+  reviews,
+  className,
+  reviewClassName,
+  msPerPixel = 0,
+}: {
+  reviews?: string[];
+  className?: string;
+  reviewClassName?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+} = {}) => {
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  return <div ref={columnRef} className={cn()}>
+
+  </div>
 };
 
 const Review = () => {
